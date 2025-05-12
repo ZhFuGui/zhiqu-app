f0b4611ae158928d64834693d0efeabdc8deff8e
ZhFuGui <17888677972@163.com>
2025-05-12 08:44:22 +0800

    a

---
+++ backend/core/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const logger = require('./logger/logger');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+const app = express();
+app.use(express.json());
+
+
+// 初始化日志
+logger.init().then(() => {
+  console.log('Logger initialized');
+});
+
+// 记录所有请求
+app.use((req, res, next) => {
+  const data = `${req.ip} ${req.method} ${req.url} `;
+  logger.access(data);
+  next();
+});
+
+const PORT = process.env.PORT || 13100;
+
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
+});
