df211b68835c8d5eea7ba25f9049e9094c60d514
ZhFuGui <17888677972@163.com>
2025-05-18 23:39:07 +0800

    完善app.js

--- backend/core/app.js
+++ backend/core/app.js
@@ -8,7 +8,7 @@ const apiRouter = require("./api/api");
 
 // 初始化日志
 logger.init().then(() => {
-  console.log("Logger initialized");
+  console.log("Logger 已经初始化");
 });
 
 // 记录所有请求
@@ -24,10 +24,20 @@ app.use("/api", apiRouter);
 const PORT = process.env.PORT;
 
 if (!PORT) {
-  logger.info(`没有找到指定 PORT`);
-  process.exit(1);
+  logger.error(`没有找到指定 PORT`).then(() => {
+    process.exit(1);
+  })
 }
 
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  logger.info(`服务器启动成功，监听端口: ${PORT}`);
+});
+
+server.on('error', async (err) => {
+  if (err.code === 'EADDRINUSE') {
+    await logger.error(`端口 ${PORT} 已经被占用`);
+  } else {
+    await logger.error(`服务器启动失败:  ${err.message}`);
+  }
+  process.exit(1);
 });
